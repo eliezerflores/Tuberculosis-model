5770f31e768de6c864158dbb50c310e90c4e8080
eliezerflores <elie.zer@live.com.mx>
2023-04-13 18:37:33 -0600

    Add files via upload

---
+++ Bifurcation analysis of Tb model_2023/Bifurcation analysis/Plots_bifurcations.m
@@ -0,0 +1,113 @@
+matrizk6= matriz;
+
+%%
+figure(1)
+hold on
+plot(log(matrizk6(:,1)),matrizk6(:,10),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(log(matrizk6(:,1)),matrizk6(:,12),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(log(matrizk6(:,1)),matrizk6(:,14),'o','MarkerSize', 5,'MarkerEdgeColor', 'b');
+hold on
+plot(log(matrizk6(:,1)),matrizk6(:,4),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
+hold on
+plot(log(matrizk6(:,1)),matrizk6(:,6),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
+
+%ylim([18979613.7030956,24979613.7030956])
+%ylim([0,24979000])
+%xlim([1,5])
+
+
+
+%%
+figure(1)
+hold on
+plot(log(lados(:,1)),lados(:,10),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(log(lados(:,1)),lados(:,12),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(log(lados(:,1)),lados(:,14),'o','MarkerSize', 5,'MarkerEdgeColor', 'b');
+hold on
+plot(log(lados(:,1)),lados(:,4),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
+hold on
+plot(log(lados(:,1)),lados(:,6),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
+
+%ylim([18979613.7030956,24979613.7030956])
+%ylim([0,24979000])
+%xlim([0,6.5])
+
+
+
+%%
+
+
+figure(1)
+hold on
+plot(log(matrizk10(:,1)),matrizk10(:,10),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(log(matrizk10(:,1)),matrizk10(:,12),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(log(matrizk10(:,1)),matrizk10(:,14),'o','MarkerSize', 5,'MarkerEdgeColor', 'b');
+hold on
+plot(log(matrizk10(:,1)),matrizk10(:,4),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
+hold on
+plot(log(matrizk10(:,1)),matrizk10(:,6),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
+
+%ylim([18979613.7030956,24979613.7030956])
+%ylim([0,24979000])
+xlim([0,6.5])
+
+
+
+
+%%
+figure(1)
+hold on
+plot(matrizk6(:,1),matrizk6(:,10),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(matrizk6(:,1),matrizk6(:,12),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(matrizk6(:,1),matrizk6(:,14),'o','MarkerSize', 5,'MarkerEdgeColor', 'b');
+hold on
+plot(matrizk6(:,1),matrizk6(:,4),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
+hold on
+plot(matrizk6(:,1),matrizk6(:,6),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
+
+%ylim([18979613.7030956,24979613.7030956])
+%ylim([0,24979000])
+%xlim([1160000,19770072])
+
+
+
+%%
+figure(1)
+hold on
+plot(lados(:,1),lados(:,10),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(lados(:,1),lados(:,12),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(lados(:,1),lados(:,14),'o','MarkerSize', 5,'MarkerEdgeColor', 'b');
+hold on
+plot(lados(:,1),lados(:,4),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
+hold on
+plot(lados(:,1),lados(:,6),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
+
+%ylim([18979613.7030956,24979613.7030956])
+%ylim([0,24979000])
+xlim([1160000,19770072])
+
+
+
+
+%%
+figure(2)
+hold on
+plot(log(matrizk6(:,1)),matrizk6(:,10),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(log(matrizk6(:,1)),log(matrizk6(:,12)),'o','MarkerSize', 5, 'MarkerEdgeColor', 'b');
+hold on
+plot(log(matrizk6(:,1)),log(matrizk6(:,14)),'o','MarkerSize', 5,'MarkerEdgeColor', 'b');
+hold on
+plot(log(matrizk6(:,1)),log(matrizk6(:,4)),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
+hold on
+plot(log(matrizk6(:,1)),log(matrizk6(:,6)),'o','MarkerSize', 7, 'MarkerFaceColor', 'r', 'MarkerEdgeColor', 'r');
